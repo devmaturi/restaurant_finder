b52fd05b9de220d2031e452274d02228545cb031
ucwealth <ucheokeke16@gmail.com>
2020-06-17 16:40:53 +0100

    added containers

---
+++ frontend/gui/src/containers/RestaurantListView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Restaurant from '../components/Restaurant';
+
+class RestaurantList extends React.Component {
+    render(){
+        return (
+            <Restaurant />
+        );
+    }
+}
+export default RestaurantList;
