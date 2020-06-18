e7361638eac2f8e8bc9c2e5f6bea01ed102fcdbe
ucwealth <ucheokeke16@gmail.com>
2020-06-18 02:33:49 +0100

    added the data fetch

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/Layout';
 import RestaurantList from './containers/RestaurantListView';
--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -7,19 +7,19 @@ const CustomLayout = (props) => {
             <Layout className="layout">
               <Header>
                 <div className="logo" />
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
                   <Menu.Item key="1">nav 1</Menu.Item>
                   <Menu.Item key="2">nav 2</Menu.Item>
                   <Menu.Item key="3">nav 3</Menu.Item>
                 </Menu>
               </Header>
-              <Content style={{ padding: '0 50px' }}>
+              <Content style={{ padding: '0 80px' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
                   <Breadcrumb.Item>Home</Breadcrumb.Item>
                   <Breadcrumb.Item>List</Breadcrumb.Item>
                   <Breadcrumb.Item>App</Breadcrumb.Item>
                 </Breadcrumb>
-                    <div className="site-layout-content">
+                    <div className="site-layout-content" style={{background: '#fff', padding: '30px'}}>
                         {props.children}
                     </div>
               </Content>
