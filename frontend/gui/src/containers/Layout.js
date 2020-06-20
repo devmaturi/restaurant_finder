4c8b8f38a3fde7fc0a550f701faa21b8871e40cb
ucwealth <ucheokeke16@gmail.com>
2020-06-20 23:46:55 +0100

    edited login

--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -12,7 +12,7 @@ const CustomLayout = (props) => {
                 <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
                   <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
                   <Menu.Item key="2"><Link to="/">Register</Link></Menu.Item>
-                  <Menu.Item key="3"><Link to="/">Login</Link></Menu.Item>
+                  <Menu.Item key="3"><Link to="/login">Login</Link></Menu.Item>
                   <Menu.Item key="4"><Link to="/">Add Restaurant</Link></Menu.Item>
                 </Menu>
               </Header>
