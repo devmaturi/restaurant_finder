bff9b89cf9a4f26a482e88da68bb8996cde93ee0
ucwealth <ucheokeke16@gmail.com>
2020-06-21 21:56:39 +0100

    create view created

--- backend/src/restaurants/api/urls.py
+++ backend/src/restaurants/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RestaurantListView, RestaurantDetailView
+from .views import RestaurantListView, RestaurantDetailView, RestaurantCreateView
 
 urlpatterns = [
     path('', RestaurantListView.as_view()),
-    path('<pk>', RestaurantDetailView.as_view()),
+    path('create/', RestaurantCreateView.as_view()),
+    path('<pk>', RestaurantDetailView.as_view())
 ]
--- backend/src/restaurants/api/views.py
+++ backend/src/restaurants/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from restaurants.models import Restaurant
 from .serializers import RestaurantSerializer
 
@@ -9,3 +9,8 @@ class RestaurantListView(ListAPIView):
 class RestaurantDetailView(RetrieveAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+class RestaurantCreateView(CreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
