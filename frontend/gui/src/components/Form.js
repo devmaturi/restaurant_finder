3c74b1066a523cea97dbb2037af92e948688c384
ucwealth <ucheokeke16@gmail.com>
2020-06-21 23:57:07 +0100

    changed Custom Form to class component

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 
-const CustomForm = () => {
+class CustomForm extends React.Component {
 
-const handleSubmit = (e) => {
-    const title = e.target.elements.title.value;
-    const description = e.target.elements.description.value;
-    console.log(title, description);
-}
-  return (
-    <div>
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Item label="Title">
-          <Input name="title" placeholder="Enter Title" />
-        </Form.Item>
-        <Form.Item label="Description">
-          <Input name="description" placeholder="Enter Restaurant Description" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">Submit</Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+    handleSubmit = (e) => {
+        const title = e.target.elements.title.value;
+        const description = e.target.elements.description.value;
+        console.log(title, description);
+    }
+    render(){
+        return (
+            <div>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Item label="Title">
+                <Input name="title" placeholder="Enter Title" />
+                </Form.Item>
+                <Form.Item label="Description">
+                <Input name="description" placeholder="Enter Restaurant Description" />
+                </Form.Item>
+                <Form.Item>
+                <Button type="primary" htmlType="submit">Submit</Button>
+                </Form.Item>
+            </Form>
+            </div>
+        );
+    }
+
 };
 
 export default CustomForm;
